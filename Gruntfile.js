a453aa6024043a4d12dfca6098ced237ead5b1a7
Karolis Kazlauskis <karolis@kazlauskis.com>
2015-01-17 13:35:39 +0000

    Update Gruntfile.js
    Removed Libs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,6 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        bower: {
-          install:{
-              options:{
-                  targetDir: './src/lib',
-                  cleanBowerDir: true
-              }
-          }
-        },
         karma: {
             unit: {
                 configFile: 'test/karma.conf.js'
@@ -42,23 +34,6 @@ module.exports = function(grunt) {
                 // the location of the resulting JS file
                 dest: 'dist/<%= pkg.name %>.js'
             },
-
-            src_lib: {
-                // the files to concatenate
-                src: [
-                    'src/lib/IndexedDBShim/IndexedDBShim.js',
-                    'src/lib/latlon/vector3d.js',
-                    'src/lib/latlon/geo.js',
-                    'src/lib/latlon/latlon-ellipsoid.js',
-                    'src/lib/latlon/osgridref.js',
-                    'src/lib/handlebars/handlebars.js',
-                    'src/lib/photoswipe/lib/klass.min.js',
-                    'src/lib/photoswipe/code.photoswipe.jquery-3.0.5.min.js'
-
-                ],
-                // the location of the resulting JS file
-                dest: 'dist/<%= pkg.name %>.libs.js'
-            }
         },
         replace: {
             version: {
@@ -72,17 +47,6 @@ module.exports = function(grunt) {
                     }
                 ]
             },
-            use_strict: {
-                src: [
-                    'dist/<%= pkg.name %>.libs.js'
-                ],
-                overwrite: true,                 // overwrite matched source files
-                replacements: [{
-                        from: /[\'"]use strict[\'"];/g,                   // string replacement
-                        to: ''
-                    }
-                ]
-            }
         },
         uglify: {
             options: {
@@ -98,14 +62,12 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-text-replace');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // the default task can be run just by typing "grunt" on the command line
-    grunt.registerTask('init', ['bower']);
     grunt.registerTask('build', ['concat', 'replace', 'uglify']);
-    grunt.registerTask('default', ['init', 'build']);
+    grunt.registerTask('default', ['build']);
 
 };
