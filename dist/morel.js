2e95255f0489fbf81a18d6073c1501fa57421c6f
kazlauskis <karolis@kazlauskis.com>
2015-02-16 13:03:24 +0000

    Updated dist.

--- dist/morel.js
+++ dist/morel.js
@@ -245,7 +245,7 @@ morel.extend('io', function (m) {
    * @returns {*}
    */
   m.getRecordURL = function (basePath) {
-    return basePath + m.CONF.RECORD_URL;
+    return m.CONF.RECORD_URL;
   };
 
   return m;
