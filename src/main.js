82c457e253e61ce89c041ad65ce3768ae2b42760
kazlauskis <karolis@kazlauskis.com>
2015-06-30 13:08:07 +0100

    Added basic AMD

--- src/main.js
+++ src/main.js
@@ -1,92 +1,115 @@
 /*global _log*/
-var morel = (function () {
-  /*
-   * Things to work on:
-   *  - Decouple the modules as much as possible
-   *  - Close as many global variables
-   */
-  "use strict";
 
-  var m = {};
-  m.VERSION = '0'; //library version, generated/replaced by grunt
+(function (factory) {
+  //Following Backbone.js header style
 
-  //library wide configuration
-  m.CONF = {};
+  // Establish the root object, `window` (`self`) in the browser, or `global` on the server.
+  // We use `self` instead of `window` for `WebWorker` support.
+  var root = (typeof self == 'object' && self.self == self && self) ||
+    (typeof global == 'object' && global.global == global && global);
 
-  //CONSTANTS:
-  m.TRUE = 1;
-  m.FALSE = 0;
-  m.ERROR = -1;
+  //AMD
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery', 'exports'], function ($, exports) {
+      root.morel = factory(root, exports, $);
+    });
 
-  m.SETTINGS_KEY =  'morel-settings';
+  //Node.js or CommonJS
+  } else if (typeof exports !== 'undefined') {
+    try { $ = require('jquery');} catch (e) {}
+    factory(root, exports, $);
 
-  /**
-   * Extends the morel library with the provided namespace and its object.
-   *
-   * @param name
-   * @param obj
-   * @returns {*|{}}
-   */
-  m.extend = function (name, obj) {
-    var nameArray = name.split('.');
-    var variable = m[nameArray[0]] = m[nameArray[0]] || {};
+  //Browser global
+  } else {
+    root.morel = factory(root, {}, (root.$ || root.jQuery));
+  }
+}(function (root, m, $) {
+    /*
+     * Things to work on:
+     *  - Decouple the modules as much as possible
+     *  - Close as many global variables
+     */
+    "use strict";
 
-    //iterate through the namespaces
-    for (var i = 1; i < nameArray.length; i++) {
-      if (variable[nameArray[i]] !== 'object') {
-        //overwrite if it is not an object
-        variable[nameArray[i]] = {};
+    m.VERSION = '0'; //library version, generated/replaced by grunt
+
+    //library wide configuration
+    m.CONF = {};
+
+    //CONSTANTS:
+    m.TRUE = 1;
+    m.FALSE = 0;
+    m.ERROR = -1;
+
+    m.SETTINGS_KEY =  'morel-settings';
+
+    /**
+     * Extends the morel library with the provided namespace and its object.
+     *
+     * @param name
+     * @param obj
+     * @returns {*|{}}
+     */
+    m.extend = function (name, obj) {
+      var nameArray = name.split('.');
+      var variable = m[nameArray[0]] = m[nameArray[0]] || {};
+
+      //iterate through the namespaces
+      for (var i = 1; i < nameArray.length; i++) {
+        if (variable[nameArray[i]] !== 'object') {
+          //overwrite if it is not an object
+          variable[nameArray[i]] = {};
+        }
+        variable = variable[nameArray[i]];
       }
-      variable = variable[nameArray[i]];
-    }
-    //if a function than initialize it otherwise assign an object
-    variable = typeof(obj) === "function" ? obj(variable) : obj || {};
-    return variable;
-  };
+      //if a function than initialize it otherwise assign an object
+      variable = typeof(obj) === "function" ? obj(variable) : obj || {};
+      return variable;
+    };
 
-  /**
-   * Initialises the application settings.
-   */
-  m.initSettings = function () {
-    morel.storage.set(m.SETTINGS_KEY, {});
-  };
+    /**
+     * Initialises the application settings.
+     */
+    m.initSettings = function () {
+      morel.storage.set(m.SETTINGS_KEY, {});
+    };
 
-  /**
-   * Sets an app setting.
-   *
-   * @param item
-   * @param data
-   * @returns {*}
-   */
-  m.settings = function (item, data) {
-    var settings = morel.storage.get(m.SETTINGS_KEY);
-    if (!settings) {
-      morel.initSettings();
-      settings = morel.storage.get(m.SETTINGS_KEY);
-    }
+    /**
+     * Sets an app setting.
+     *
+     * @param item
+     * @param data
+     * @returns {*}
+     */
+    m.settings = function (item, data) {
+      var settings = morel.storage.get(m.SETTINGS_KEY);
+      if (!settings) {
+        morel.initSettings();
+        settings = morel.storage.get(m.SETTINGS_KEY);
+      }
 
-    if (data) {
-      settings[item] = data;
-      return morel.storage.set(m.SETTINGS_KEY, settings);
-    } else {
-      return (item) ? settings[item] : settings;
-    }
-  };
+      if (data) {
+        settings[item] = data;
+        return morel.storage.set(m.SETTINGS_KEY, settings);
+      } else {
+        return (item) ? settings[item] : settings;
+      }
+    };
 
-  /**
-   * Resets the morel to the initial state.
-   *
-   * Clears localStorage.
-   * Clears sessionStorage.
-   * Clears databases.
-   */
-  m.reset = function () {
-    morel.storage.clear();
-    morel.storage.tmpClear();
+    /**
+     * Resets the morel to the initial state.
+     *
+     * Clears localStorage.
+     * Clears sessionStorage.
+     * Clears databases.
+     */
+    m.reset = function () {
+      morel.storage.clear();
+      morel.storage.tmpClear();
 
-    //morel.db.clear();
-    morel.record.db.clear();
-  };
+      //morel.db.clear();
+      morel.record.db.clear();
+    };
 
-  return m;
-})(); //END
+    return m;
+}));
