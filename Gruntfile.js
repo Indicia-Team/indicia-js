09346541cbec0227e751d9176b97127ea1b8fb3d
kazlauskis <kazlauskis>
2015-11-28 17:40:03 +0000

    Create dist directory and add Sauce Labs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,105 +1,110 @@
 module.exports = function (grunt) {
-    'use strict';
-    var banner = "/*!\n" +
-      " * <%= pkg.name %> <%= pkg.version %>\n" +
-      " * <%= pkg.description %> \n" +
-        " *\n" +
-        " * <%= pkg.homepage %>\n" +
-        " *\n" +
-        " * Author <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
-        " * Released under the <%= _.pluck(pkg.licenses, 'type').join(', ') %> license.\n" +
-        " * <%= _.pluck(pkg.licenses, 'url') %>\n" +
-        " */\n";
+  'use strict';
+  var banner = "/*!\n" +
+    " * <%= pkg.name %> <%= pkg.version %>\n" +
+    " * <%= pkg.description %> \n" +
+    " *\n" +
+    " * <%= pkg.homepage %>\n" +
+    " *\n" +
+    " * Author <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
+    " * Released under the <%= _.pluck(pkg.licenses, 'type').join(', ') %> license.\n" +
+    " * <%= _.pluck(pkg.licenses, 'url') %>\n" +
+    " */\n";
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  var DIST_LOC = 'dist/';
 
-        requirejs: {
-            compile: {
-                options: {
-                    baseUrl: "src",
-                    optimize: "none",
-                    findNestedDependencies: true,
-                    skipModuleInsertion: true,
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-                    include: [
-                        "main",
-                        "Manager",
-                        "Geoloc"
-                    ],
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "src",
+          optimize: "none",
+          findNestedDependencies: true,
+          skipModuleInsertion: true,
 
-                    out: '<%= pkg.name %>.js',
+          include: [
+            "main",
+            "Manager",
+            "Geoloc"
+          ],
 
-                    pragmasOnSave: {
-                        buildExclude: true
-                    },
+          out: DIST_LOC + '<%= pkg.name %>.js',
 
-                    wrap: {
-                        startFile: "src/wrap.start",
-                        endFile: "src/wrap.end"
-                    }
-                }
-            }
-        },
+          pragmasOnSave: {
+            buildExclude: true
+          },
 
-        concat: {
-            options: {
-                // define a string to put between each file in the concatenated output
-                separator: '\n\n'
-            },
-            src: {
-                options: {
-                    banner: banner
-                },
-                // the file to update
-                src: '<%= pkg.name %>.js',
-
-                // the location of the resulting JS file
-                dest: '<%= pkg.name %>.js'
-            }
-        },
-
-        replace: {
-            version: {
-                src: [
-                    '<%= pkg.name %>.js'
-                ],
-                overwrite: true,     // overwrite matched source files
-                replacements: [{
-                    from: /(m\.VERSION =) \'0\';/g,     // string replacement
-                    to: '$1 \'<%= pkg.version %>\';'
-                }]
-            }
-        },
-
-        uglify: {
-            options: {
-                // the banner is inserted at the top of the output
-                banner: banner
-            },
-            dist: {
-                files: {
-                    '<%= pkg.name %>.min.js': ['<%= concat.src.dest %>']
-                }
-            }
-        },
-
-        karma: {
-            unit: {
-                configFile: 'test/karma.conf.js'
-            }
+          wrap: {
+            startFile: "src/wrap.start",
+            endFile: "src/wrap.end"
+          }
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-text-replace');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: '\n\n'
+      },
+      src: {
+        options: {
+          banner: banner
+        },
+        // the file to update
+        src: DIST_LOC + '<%= pkg.name %>.js',
 
-    // the default task can be run just by typing "grunt" on the command line
-    grunt.registerTask('build', ['requirejs', 'concat', 'replace', 'uglify']);
-    grunt.registerTask('test', ['karma']);
-    grunt.registerTask('default', ['build']);
+        // the location of the resulting JS file
+        dest: DIST_LOC + '<%= pkg.name %>.js'
+      }
+    },
+
+    replace: {
+      version: {
+        src: [
+          DIST_LOC + '<%= pkg.name %>.js'
+        ],
+        overwrite: true,     // overwrite matched source files
+        replacements: [{
+          from: /(m\.VERSION =) \'0\';/g,     // string replacement
+          to: '$1 \'<%= pkg.version %>\';'
+        }]
+      }
+    },
+
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: banner
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.src.dest %>']
+        }
+      }
+    },
+
+    karma: {
+      local: {
+        configFile: 'karma.conf.js'
+      },
+      sauce: {
+        configFile: 'karma.conf-sauce.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+  // the default task can be run just by typing "grunt" on the command line
+  grunt.registerTask('build', ['requirejs', 'concat', 'replace', 'uglify']);
+  grunt.registerTask('test', ['karma:local']);
+  grunt.registerTask('default', ['build']);
 
 };
