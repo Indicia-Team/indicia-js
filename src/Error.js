65cd5ea277e87b31ab4809b3a3223d2617ae357f
kazlauskis <karolis@kazlauskis.com>
2017-02-03 17:37:12 +0000

    Improve Error object

--- src/Error.js
+++ src/Error.js
@@ -4,8 +4,15 @@
 class Error {
   constructor(options = {}) {
     if (typeof options === 'string') {
+      // message only
       this.code = -1;
       this.message = options;
+      return;
+    } else if (options instanceof Array) {
+      // array of errors
+      this.message = options.reduce(
+        (message, error) => `${message}${error.title}\n`,
+        '');
       return;
     }
 
