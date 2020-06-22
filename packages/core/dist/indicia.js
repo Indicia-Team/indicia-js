ffef4e0cda14ebc243c274a317bbd05dbb967140
Karolis Kazlauskis <karolis@flumens.io>
2020-06-22 11:27:02 +0100

    v5.2.2

--- packages/core/dist/indicia.js
+++ packages/core/dist/indicia.js
@@ -1,5 +1,5 @@
 /*!
- * @indicia-js/core 5.2.1
+ * @indicia-js/core 5.2.2
  * Indicia JavaScript SDK.
  * https://github.com/Indicia-Team/indicia-js
  * Author undefined
@@ -2228,8 +2228,8 @@
   function _objectSpread$3(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys$3(Object(source), true).forEach(function (key) { defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys$3(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
   var Indicia = _objectSpread$3({
-    /* global "5.2.1" */
-    VERSION: "5.2.1",
+    /* global "5.2.2" */
+    VERSION: "5.2.2",
     // replaced by build
     Sample: Sample,
     Occurrence: Occurrence,
